9f652e730f6e488a0110d2cf781f1d1233053c1f
29furkhan <29furkhanshaikh@gmail.com>
2024-10-23 17:55:36 +0530

    Basic changes added

--- src/main/scala/com/sundogsoftware/spark/HelloWorld.scala
+++ src/main/scala/com/sundogsoftware/spark/HelloWorld.scala
@@ -13,7 +13,7 @@ object HelloWorld {
     val lines = sc.textFile("data/ml-100k/u.data")
     val numLines = lines.count()
 
-    println("Hello furkhan! The u.data file has " + numLines + " lines.")
+    println("Hello, furkhan shaikh! The u.data file has " + numLines + " lines.")
 
     sc.stop()
   }
