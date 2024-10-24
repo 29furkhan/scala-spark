a3c1182d481a3f4c71bf334f2a1b5121164504b4
furkhan1999 <me.furkhan.shaikh@gmail.com>
2024-10-24 21:01:17 +0530

    DF/ DS skeleton added

--- src/main/scala/com/furkhan/examples/DFs/FurkhanSparkSQLDataset.scala
+++ src/main/scala/com/furkhan/examples/DFs/FurkhanSparkSQLDataset.scala
@@ -7,12 +7,11 @@ object FurkhanSparkSQLDataset {
 
   case class Person(id:Int, name:String, age:Int, friends:Int)
 
-  /** Our main function where the action happens */
   def main(args: Array[String]) {
-    
+
     // Set the log level to only print errors
     Logger.getLogger("org").setLevel(Level.ERROR)
-    
+
     // Use SparkSession interface
     val spark = SparkSession
       .builder
@@ -29,15 +28,13 @@ object FurkhanSparkSQLDataset {
       .as[Person]
 
     schemaPeople.printSchema()
-    
+
     schemaPeople.createOrReplaceTempView("people")
 
-    val teenagers = spark.sql("SELECT * FROM people WHERE age >= 13 AND age <= 19")
-    
-    val results = teenagers.collect()
-    
-    results.foreach(println)
-    
+    val teenagers = spark.sql("SELECT * FROM people WHERE age >= 13 AND age <= 19").as[Person]
+
+    teenagers.show()
+
     spark.stop()
   }
 }
